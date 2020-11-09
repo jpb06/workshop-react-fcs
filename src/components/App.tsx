10c95a8950e62d98ef13ea5a042e774504d2957a
jpb06 <jp.bois.06@outlook.fr>
2020-11-09 16:56:59 +0100

    Updating CRA

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,7 +6,8 @@ import { CssBaseline, Grid, MuiThemeProvider } from "@material-ui/core";
 import { appTheme } from "../create.theme";
 import configureStore from "../redux/store/configure.store";
 import { MyDevFriends } from "./dev-friends/MyDevFriends";
-import { MyDevFriendsClass } from "./dev-friends/MyDevFriendsClass";
+
+//import { MyDevFriendsClass } from "./dev-friends/MyDevFriendsClass";
 
 const store = configureStore();
 
@@ -15,8 +16,8 @@ export const App = () => (
     <MuiThemeProvider theme={appTheme}>
       <CssBaseline />
       <Grid container direction="row" justify="center" alignItems="center">
-        <MyDevFriendsClass />
-        {/* <MyDevFriends /> */}
+        {/* <MyDevFriendsClass /> */}
+        <MyDevFriends /> 
       </Grid>
     </MuiThemeProvider>
   </ReduxProvider>
