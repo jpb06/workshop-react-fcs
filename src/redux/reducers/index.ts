ca1d215c0d902951fb33e9e902899b2a43017db2
jpb06 <jpb.06@outlook.com>
2021-01-11 19:40:21 +0100

    Typing root reducer

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,11 +1,12 @@
 import { combineReducers } from "redux";
 
+import { RootState } from "../store/root.state";
 import { devsReducer } from "./devs.reducer";
 import { isAppLoadingReducer } from "./is.app.loading.reducer";
 import { selectedSquadsReducer } from "./selected.squads.reducer";
 import { squadsReducer } from "./squads.reducer";
 
-export const rootReducer = combineReducers({
+export const rootReducer = combineReducers<RootState>({
   selectedSquads: selectedSquadsReducer,
   squads: squadsReducer,
   devs: devsReducer,
