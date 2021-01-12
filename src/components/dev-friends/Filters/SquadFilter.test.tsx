0f6a67d7ad116eadc81b5f129ba497dc6571f29f
jpb06 <jpb.06@outlook.com>
2021-01-12 22:05:30 +0100

    Mocking fake api calls to avoid the hardcoded delay

--- src/components/dev-friends/Filters/SquadFilter.test.tsx
+++ src/components/dev-friends/Filters/SquadFilter.test.tsx
@@ -1,15 +1,23 @@
 import React from "react";
+import { mocked } from "ts-jest/dist/utils/testing";
 
 import { screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
+import { getDevs, getSquads } from "../../../mockApi/api";
+import { devs, squads } from "../../../mockApi/data";
 import { connectedRender } from "../../../tests-related/connected.render";
 import { SquadFilter } from "./SquadFilter";
+
+jest.mock("../../../mockApi/api");
 
 describe("Squad filter component", () => {
   console.log = jest.fn(); // suppressing hooks logs
 
-  it("should display no checkboxes when squads are loading", () => {
+  mocked(getSquads).mockResolvedValue(squads);
+  mocked(getDevs).mockResolvedValue(devs);
+
+  it("should display no checkboxes when squads are loading", async () => {
     connectedRender(<SquadFilter />);
 
     expect(screen.queryAllByRole("checkbox")).toHaveLength(0);
@@ -25,13 +33,9 @@ describe("Squad filter component", () => {
     connectedRender(<SquadFilter />);
 
     for (let i = 1; i < 5; i++) {
-      const checkbox = await screen.findByRole(
-        "checkbox",
-        {
-          name: new RegExp(`squad ${i}`, "i"),
-        },
-        { timeout: 2000 }
-      );
+      const checkbox = await screen.findByRole("checkbox", {
+        name: new RegExp(`squad ${i}`, "i"),
+      });
       expect(checkbox).toBeChecked();
     }
   });
@@ -39,13 +43,9 @@ describe("Squad filter component", () => {
   it("should uncheck on user click", async () => {
     connectedRender(<SquadFilter />);
 
-    const squad1Checkbox = await screen.findByRole(
-      "checkbox",
-      {
-        name: /squad 1/i,
-      },
-      { timeout: 2000 }
-    );
+    const squad1Checkbox = await screen.findByRole("checkbox", {
+      name: /squad 1/i,
+    });
     userEvent.click(squad1Checkbox);
 
     expect(squad1Checkbox).not.toBeChecked();
