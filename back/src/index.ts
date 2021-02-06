dfa14036ad9de0d80fccb1b6d8d57ccd1a279696
jpb06 <jpb.06@outlook.com>
2021-02-06 10:19:17 +0100

    Pluggin everything in

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,19 +1,31 @@
 import cors from "cors";
 import express, { Express } from "express";
 
+import { contextInitializationMiddleware } from "@middlewares/context.initialization.middleware";
+import { errorsMiddleware } from "@middlewares/errors.middleware";
+import { noRouteMiddleware } from "@middlewares/no.route.middleware";
+import { responseMiddlewares } from "@middlewares/response.middlewares";
+
+import { createMockDb } from "./dal/mockdb/logic/create.mock.db";
 import { artificialDelayMiddleware } from "./middlewares/artificial.delay.middleware";
 import { mainRouter } from "./routes";
+
+createMockDb();
 
 const app: Express = express();
 
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(responseMiddlewares);
 
 // simulate delay
 app.use(artificialDelayMiddleware);
 
-app.use(mainRouter);
+app.use(contextInitializationMiddleware, mainRouter);
+
+app.use(errorsMiddleware);
+app.use(noRouteMiddleware);
 
 const port = 3001;
 app.listen(port, "", () => {
