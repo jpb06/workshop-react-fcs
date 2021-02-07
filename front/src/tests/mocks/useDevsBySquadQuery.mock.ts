29206bc40d98f44992b1748b2d24a276ecdf2450
jpb06 <jpb.06@outlook.com>
2021-02-07 14:48:50 +0100

    Renaming folder: tests -> tests-related

--- front/src/tests/mocks/useDevsBySquadQuery.mock.ts
+++
@@ -1,6 +0,0 @@
-import { mocked } from "ts-jest/utils";
-
-import { useDevsBySquadQuery } from "@api/useDevsBySquadQuery.hook";
-
-export const setUseDevsBySquadReturnValue = (data: any) =>
-  mocked(useDevsBySquadQuery).mockReturnValueOnce({ data } as any);
--- front/src/tests/mocks/useSquadQuery.mock.ts
+++
@@ -1,6 +0,0 @@
-import { mocked } from "ts-jest/utils";
-
-import { useSquadsQuery } from "@api/useSquadsQuery.hook";
-
-export const setUseSquadsReturnValue = (data: any) =>
-  mocked(useSquadsQuery).mockReturnValue({ data } as any);
