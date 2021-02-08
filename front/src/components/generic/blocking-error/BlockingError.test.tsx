4da4699d83485c5a3275e99f8d2f02422623496a
jpb06 <jpb.06@outlook.com>
2021-02-08 18:50:05 +0100

    Adding an example for conditional styling (clsx)

---
+++ front/src/components/generic/blocking-error/BlockingError.test.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import { render, screen } from "@testing-library/react";
+
+import { BlockingError } from "./BlockingError";
+
+describe("Blocking error component", () => {
+  it("should display a title", () => {
+    render(<BlockingError title="Yolo!" content="Something something" />);
+
+    screen.getByText(/yolo!/i);
+  });
+
+  it("should display a message", () => {
+    render(<BlockingError title="Yolo!" content="Something something" />);
+
+    screen.getByText(/something something/i);
+  });
+
+  it("should display an icon", () => {
+    render(<BlockingError title="Yolo!" content="Something something" />);
+
+    screen.getByRole("errorimg", {
+      hidden: true,
+    });
+  });
+});
