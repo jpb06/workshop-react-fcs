743c94ba427c62bc936f7bcef60e0407355664f0
jpb06 <jpb.06@outlook.com>
2021-02-13 11:22:33 +0100

    Unneeded export

--- back/src/logic/objectId.logic.ts
+++ back/src/logic/objectId.logic.ts
@@ -1,6 +1,6 @@
 import { ObjectId } from "bson";
 
-export const hexString = () => {
+const hexString = () => {
   var timestamp = ((new Date().getTime() / 1000) | 0).toString(16);
   return (
     timestamp +
