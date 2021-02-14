c0248409a521627646466ac51ae7e57355c17631
jpb06 <jpb.06@outlook.com>
2021-02-14 13:07:47 +0100

    Removing the action area in the layout (useless)

--- front/src/components/generic/page-layout/PageLayout.tsx
+++ front/src/components/generic/page-layout/PageLayout.tsx
@@ -3,14 +3,7 @@ import React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 import {
-  Card,
-  CardActionArea,
-  CardContent,
-  CardMedia,
-  CssBaseline,
-  Grid,
-  ThemeProvider,
-  Typography,
+    Card, CardContent, CardMedia, CssBaseline, Grid, ThemeProvider, Typography
 } from "@material-ui/core";
 import { appTheme } from "@src/create.theme";
 
@@ -57,27 +50,21 @@ export const PageLayout: React.FC<PageLayoutProps> = ({ children }) => {
           <CssBaseline />
           <Grid container direction="row" justify="center" alignItems="center">
             <Card className={classes.card}>
-              <CardActionArea>
-                <CardMedia
-                  className={classes.media}
-                  image="/happy.jpg"
-                  title="Friends"
-                />
-                <GlobalLoadingIndicator />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    My dev friends
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                  >
-                    They are cool and good; but also great and cool. Together we
-                    build sand castles with our minds.
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
+              <CardMedia
+                className={classes.media}
+                image="/happy.jpg"
+                title="Friends"
+              />
+              <GlobalLoadingIndicator />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  My dev friends
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  They are cool and good; but also great and cool. Together we
+                  build sand castles with our minds.
+                </Typography>
+              </CardContent>
               {children}
             </Card>
           </Grid>
