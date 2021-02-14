90bf2df1887a3607897ff3b2b48b87b1f5eb82a9
jpb06 <jpb.06@outlook.com>
2021-02-14 13:14:34 +0100

    Creating a modal to change the squad of a dev

---
+++ front/src/components/dev-friends/change-squad-modal/ChangeSquadModal.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+
+import { BlockingError } from "@components/generic/blocking-error/BlockingError";
+import { CircularLoading } from "@components/generic/circular-loading/CircularLoading";
+import { DownTransition } from "@components/generic/down-transition/DownTransition";
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from "@material-ui/core";
+import { Dev } from "@sharedtypes/dev.interface";
+
+import { useModalActions } from "./hooks/useModalActions.hook";
+import { IdleState } from "./states/IdleState";
+
+interface ChangeSquadModalProps {
+  onClose: () => void;
+  isOpen: boolean;
+  dev?: Dev;
+}
+
+export const ChangeSquadModal: React.FC<ChangeSquadModalProps> = ({
+  onClose,
+  isOpen,
+  dev,
+}): JSX.Element => {
+  const { handleSquadChanged, handleCancel, status } = useModalActions(
+    onClose,
+    dev
+  );
+
+  if (!dev) return null;
+
+  return (
+    <Dialog
+      disableBackdropClick
+      disableEscapeKeyDown
+      maxWidth="xs"
+      aria-labelledby="change-squad"
+      open={isOpen}
+      TransitionComponent={DownTransition}
+    >
+      <DialogTitle id="change-squad-title">
+        Move {dev.firstName} to another squad
+      </DialogTitle>
+      <DialogContent dividers>
+        {
+          {
+            idle: <IdleState dev={dev} onSquadChanged={handleSquadChanged} />,
+            loading: <CircularLoading />,
+            error: (
+              <BlockingError
+                title="Oh no!"
+                content={`We were unable to modify ${dev.firstName}'s squad. Sorry!`}
+              />
+            ),
+          }[status]
+        }
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleCancel} color="primary">
+          Nevermind
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
